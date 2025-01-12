c4b49a3f38cfc7d61150ee516f5c46637a7ee0f4
Alan Martines <alancpmartines@hotmail.com>
2025-01-12 19:57:15 -0400

    Update

--- backend/src/database/seeds/20200904070005-create-default-company.ts
+++ backend/src/database/seeds/20200904070005-create-default-company.ts
@@ -8,11 +8,92 @@ module.exports = {
           "Plans",
           [
             {
-              name: "Plano 1",
-              users: 10,
-              connections: 10,
+              name: "Plano Essencial",
+              users: 1,
+              connections: 1,
+              queues: 3,
+              value: 49.99,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+              name: "Plano Avançado",
+              users: 3,
+              connections: 1,
+              queues: 5,
+              value: 89.99,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+              name: "Plano Empresarial",
+              users: 5,
+              connections: 2,
+              queues: 8,
+              value: 149.99,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+              name: "Plano Executivo",
+              users: 7,
+              connections: 4,
               queues: 10,
-              value: 30,
+              value: 279.99,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+              name: "Plano Corporativo",
+              users: 10,
+              connections: 6,
+              queues: 15,
+              value: 389.99,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+              name: "Plano Essencial Plus",
+              users: 1,
+              connections: 1,
+              queues: 3,
+              value: 69.99,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+              name: "Plano Avançado Plus",
+              users: 3,
+              connections: 1,
+              queues: 5,
+              value: 119.99,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+              name: "Plano Empresarial Plus",
+              users: 5,
+              connections: 2,
+              queues: 8,
+              value: 199.99,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+              name: "Plano Executivo Plus",
+              users: 7,
+              connections: 4,
+              queues: 10,
+              value: 329.99,
+              createdAt: new Date(),
+              updatedAt: new Date()
+            },
+            {
+              name: "Plano Corporativo Plus",
+              users: 10,
+              connections: 6,
+              queues: 15,
+              value: 459.99,
               createdAt: new Date(),
               updatedAt: new Date()
             }
@@ -25,7 +106,7 @@ module.exports = {
             {
               name: "Empresa 1",
               planId: 1,
-              dueDate: "2093-03-14 04:00:00+01",
+              dueDate: "3000-01-01 04:00:00+01",
               createdAt: new Date(),
               updatedAt: new Date()
             }
