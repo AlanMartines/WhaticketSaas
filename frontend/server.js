1424384892d512927098759908ecd7cb54eb991b
Alan Martines <alancpmartines@hotmail.com>
2025-01-13 07:47:17 -0400

    Update

--- frontend/server.js
+++ frontend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const app = express();
-const { logger } = require("./src/utils/logger");
+const { logger } = require("./utils/logger");
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
