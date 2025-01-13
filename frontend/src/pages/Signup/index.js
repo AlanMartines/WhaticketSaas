b0b5e32c8ba0152adbbde7554b463db2c6b2212b
Alan Martines <alancpmartines@hotmail.com>
2025-01-13 11:26:00 -0400

    Update

--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -21,10 +21,10 @@ import {
   makeStyles,
 } from "@material-ui/core";
 import Tooltip from "@material-ui/core/Tooltip";
-import InputMask from "react-input-mask";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import CheckIcon from "@material-ui/icons/Check";
 import CloseIcon from "@material-ui/icons/Close";
+import InputMask from "react-input-mask";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import logo from "../../assets/logo.png";
 import { i18n } from "../../translate/i18n";
 import { openApi } from "../../services/api";
@@ -57,20 +57,35 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const Copyright = () => (
+  <Typography variant="body2" color="textSecondary" align="center">
+    {"Copyright © "}
+    <Link color="inherit" href="#">
+      Alan Martines
+    </Link>{" "}
+    {new Date().getFullYear()}
+    {"."}
+  </Typography>
+);
+
+const UserSchema = Yup.object().shape({
+  name: Yup.string().min(2, "Muito curto!").max(50, "Muito longo!").required("Obrigatório"),
+  password: Yup.string().min(5, "Muito curto!").max(50, "Muito longo!").required("Obrigatório"),
+  email: Yup.string().email("E-mail inválido").required("Obrigatório"),
+});
+
 const SignUp = () => {
   const classes = useStyles();
   const history = useHistory();
   const [plans, setPlans] = useState([]);
   const { list: listPlans } = usePlans();
-  const dueDate = moment().add(5, "days").format();
+  const dueDate = moment().add(3, "days").format();
 
   useEffect(() => {
     const fetchPlans = async () => {
       try {
-        const fetchedPlans = await listPlans();
-        // Ordenar os planos antes de armazená-los no estado
-        const sortedPlans = fetchedPlans.sort((a, b) => a.name.localeCompare(b.name));
-        setPlans(sortedPlans);
+        const plans = await listPlans();
+        setPlans(plans);
       } catch (error) {
         toastError(error);
       }
@@ -103,12 +118,7 @@ const SignUp = () => {
         <img className={classes.logo} src={logo} alt="Logo" />
         <Formik
           initialValues={{ name: "", email: "", phone: "", password: "", planId: "" }}
-          validationSchema={Yup.object({
-						name: Yup.string().min(2, "Muito curto!").max(50, "Muito longo!").required("Obrigatório"),
-						email: Yup.string().email("E-mail inválido").required("Obrigatório"),
-						phone: Yup.string().required("Obrigatório"),
-						password: Yup.string().min(5, "Muito curto!").max(20, "Muito longo!").required("Obrigatório"),
-          })}
+          validationSchema={UserSchema}
           onSubmit={(values, actions) => {
             handleSignUp(values);
             actions.setSubmitting(false);
@@ -141,30 +151,30 @@ const SignUp = () => {
                     helperText={touched.email && errors.email}
                   />
                 </Grid>
-                <Grid item xs={12}>
-                  <Field name="phone">
-                    {({ field, meta }) => (
-                      <InputMask
-                        mask="(99) 99999-9999"
-                        value={field.value}
-                        onChange={field.onChange}
-                        onBlur={field.onBlur}
-                      >
-                        {() => (
-                          <TextField
-                            {...field}
-                            variant="outlined"
-                            fullWidth
-                            id="phone"
-                            label="Telefone"
-                            error={meta.touched && Boolean(meta.error)}
-                            helperText={meta.touched && meta.error}
-                          />
-                        )}
-                      </InputMask>
-                    )}
-                  </Field>
-                </Grid>
+								<Grid item xs={12}>
+									<Field name="phone">
+										{({ field, meta }) => (
+											<InputMask
+												mask="(99) 99999-9999"
+												value={field.value}
+												onChange={field.onChange}
+												onBlur={field.onBlur}
+											>
+												{() => (
+													<TextField
+														{...field}
+														variant="outlined"
+														fullWidth
+														id="phone"
+														label="Telefone"
+														error={meta.touched && Boolean(meta.error)}
+														helperText={meta.touched && meta.error}
+													/>
+												)}
+											</InputMask>
+										)}
+									</Field>
+								</Grid>
                 <Grid item xs={12}>
                   <Field
                     as={TextField}
@@ -188,40 +198,40 @@ const SignUp = () => {
                     id="plan-selection"
                   >
                     {plans.map((plan) => (
-                      <MenuItem key={plan.id} value={plan.id}>
-                        <Typography variant="body1" style={{ fontWeight: "bold" }}>
-                          {`${plan.name} - Atendentes: ${plan.users} - WhatsApp: ${plan.connections} - Filas: ${plan.queues} - R$ ${plan.value}`}
-                        </Typography>
-                        <div style={{ display: "flex", flexWrap: "wrap", marginTop: "0.5rem" }}>
-                          <Tooltip title="Permite criar campanhas de marketing">
-                            {plan.useCampaigns ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Campanhas
-                          </Tooltip>
-                          {" | "}
-                          <Tooltip title="Permite agendar mensagens ou tarefas">
-                            {plan.useSchedules ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Agendamentos
-                          </Tooltip>
-                          {" | "}
-                          <Tooltip title="Habilita chat interno para comunicação entre usuários">
-                            {plan.useInternalChat ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Chat Interno
-                          </Tooltip>
-                          {" | "}
-                          <Tooltip title="Acesso a APIs externas para integração com outros sistemas">
-                            {plan.useExternalApi ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} API Externa
-                          </Tooltip>
-                          {" | "}
-                          <Tooltip title="Habilita funcionalidades de Kanban para organização de tarefas">
-                            {plan.useKanban ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Kanban
-                          </Tooltip>
-                          {" | "}
-                          <Tooltip title="Integração com OpenAI para automação e IA">
-                            {plan.useOpenAi ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} OpenAI
-                          </Tooltip>
-                          {" | "}
-                          <Tooltip title="Permite integração com outros sistemas ou serviços">
-                            {plan.useIntegrations ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Integrações
-                          </Tooltip>
-                        </div>
-                      </MenuItem>
+										<MenuItem key={plan.id} value={plan.id}>
+											<Typography variant="body1" style={{ fontWeight: "bold" }}>
+												{`${plan.name} - Atendentes: ${plan.users} - WhatsApp: ${plan.connections} - Filas: ${plan.queues} - R$ ${plan.value}`}
+											</Typography>
+											<div style={{ display: "flex", flexWrap: "wrap", marginTop: "0.5rem" }}>
+												<Tooltip title="Permite criar campanhas de marketing">
+													{plan.useCampaigns ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Campanhas
+												</Tooltip>
+												{" | "}
+												<Tooltip title="Permite agendar mensagens ou tarefas">
+													{plan.useSchedules ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Agendamentos
+												</Tooltip>
+												{" | "}
+												<Tooltip title="Habilita chat interno para comunicação entre usuários">
+													{plan.useInternalChat ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Chat Interno
+												</Tooltip>
+												{" | "}
+												<Tooltip title="Acesso a APIs externas para integração com outros sistemas">
+													{plan.useExternalApi ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} API Externa
+												</Tooltip>
+												{" | "}
+												<Tooltip title="Habilita funcionalidades de Kanban para organização de tarefas">
+													{plan.useKanban ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Kanban
+												</Tooltip>
+												{" | "}
+												<Tooltip title="Integração com OpenAI para automação e IA">
+													{plan.useOpenAi ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} OpenAI
+												</Tooltip>
+												{" | "}
+												<Tooltip title="Permite integração com outros sistemas ou serviços">
+													{plan.useIntegrations ? <CheckIcon color="primary" /> : <CloseIcon color="error" />} Integrações
+												</Tooltip>
+											</div>
+										</MenuItem>
                     ))}
                   </Field>
                 </Grid>
@@ -247,14 +257,7 @@ const SignUp = () => {
         </Formik>
       </div>
       <Box mt={5}>
-        <Typography variant="body2" color="textSecondary" align="center">
-          {"Copyright © "}
-          <Link color="inherit" href="#">
-            Alan Martines
-          </Link>{" "}
-          {new Date().getFullYear()}
-          {"."}
-        </Typography>
+        <Copyright />
       </Box>
     </Container>
   );
