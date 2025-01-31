45b25983be639ff9df4b9efc4cb8936d463ac7bb
Alan Martines <alancpmartines@hotmail.com>
2025-01-31 10:49:00 -0400

    Update

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,5 +1,7 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Link as RouterLink } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -21,155 +23,149 @@ const copyrightYear = process.env.REACT_APP_COPYRIGHT_YEAR || "";
 const copyrightUrl = process.env.REACT_APP_COPYRIGHT_URL || "";
 
 const Copyright = () => {
-	return (
-		<Typography variant="body2" color="textSecondary" align="center">
-			{"Copyright © "}
-			{copyrightYear}
-			{"-"}
-			{new Date().getFullYear()}
-			{" - "}
-			<Link color="inherit" href={copyrightUrl}>
-				{copyright}
-			</Link>
-			{"."}
-		</Typography>
-	);
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright © "}
+            {copyrightYear}
+            {"-"}
+            {new Date().getFullYear()}
+            {" - "}
+            <Link color="inherit" href={copyrightUrl}>
+                {copyright}
+            </Link>
+            {"."}
+        </Typography>
+    );
 };
 
 const useStyles = makeStyles(theme => ({
-	root: {
-		width: "100vw",
-		height: "100vh",
-		//background: "linear-gradient(to right, #76EE00 , #76EE00 , #458B00)",
-		backgroundImage: "url(https://i.imgur.com/gB0s3Zf.jpeg)",
-		backgroundRepeat: "no-repeat",
-		backgroundSize: "100% 100%",
-		backgroundPosition: "center",
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		justifyContent: "center",
-		textAlign: "center",
-	},
-	paper: {
-		backgroundColor: theme.palette.login, //DARK MODE PLW DESIGN//
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		padding: "55px 30px",
-		borderRadius: "12.5px",
-	},
-	avatar: {
-		margin: theme.spacing(1),  
-		backgroundColor: theme.palette.secondary.main,
-	},
-	form: {
-		width: "100%", // Fix IE 11 issue.
-		marginTop: theme.spacing(1),
-	},
-	submit: {
-		margin: theme.spacing(3, 0, 2),
-	},
-	powered: {
-		color: "white"
-	}
+    root: {
+        width: "100vw",
+        height: "100vh",
+        backgroundImage: "url(https://i.imgur.com/gB0s3Zf.jpeg)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100% 100%",
+        backgroundPosition: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+    },
+    paper: {
+        backgroundColor: theme.palette.login,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "55px 30px",
+        borderRadius: "12.5px",
+    },
+    form: {
+        width: "100%",
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    }
 }));
 
 const UserSchema = Yup.object().shape({
-	email: Yup.string().email("E-mail inválido").required("Obrigatório"),
-	password: Yup.string().min(5, "Muito curto!").max(50, "Muito longo!").required("Obrigatório"),
+    email: Yup.string().email("E-mail inválido").required("Obrigatório"),
+    password: Yup.string()
+        .min(5, "Muito curto!")
+        .max(50, "Muito longo!")
+        .required("Obrigatório"),
 });
 
 const Login = () => {
-	const classes = useStyles();
-	const [user, setUser] = useState({ email: "", password: "" });
-	const { handleLogin } = useContext(AuthContext);
-	const handleChangeInput = e => {
-		setUser({ ...user, [e.target.name]: e.target.value });
-	};
+    const classes = useStyles();
+    const { handleLogin } = useContext(AuthContext);
 
-	const handlSubmit = e => {
-		e.preventDefault();
-		handleLogin(user);
-	};
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: UserSchema,
+        onSubmit: (values) => {
+            handleLogin(values);
+        },
+    });
 
-	return (
-		<div className={classes.root}>
-		<Container component="main" maxWidth="xs">
-			<CssBaseline/>
-			<div className={classes.paper}>
-				<div>
-					<img style={{ margin: "0 auto", width: "100%" }} src={logo} alt="Whats" />
-				</div>
-				{/*<Typography component="h1" variant="h5">
-					{i18n.t("login.title")}
-				</Typography>*/}
-				<form className={classes.form} noValidate onSubmit={handlSubmit}>
-					<TextField
-						variant="outlined"
-						margin="normal"
-						required
-						fullWidth
-						id="email"
-						label={i18n.t("login.form.email")}
-						name="email"
-						value={user.email}
-						onChange={handleChangeInput}
-						autoComplete="email"
-						autoFocus
-					/>
-					<TextField
-						variant="outlined"
-						margin="normal"
-						required
-						fullWidth
-						name="password"
-						label={i18n.t("login.form.password")}
-						type="password"
-						id="password"
-						value={user.password}
-						onChange={handleChangeInput}
-						autoComplete="current-password"
-					/>
-					
-					<Grid container justify="flex-end">
-					  <Grid item xs={6} style={{ textAlign: "right" }}>
-						<Link component={RouterLink} to="/forgetpsw" variant="body2">
-						  Esqueceu sua senha?
-						</Link>
-					  </Grid>
-					</Grid>
-					
-					<Button
-						type="submit"
-						fullWidth
-						variant="contained"
-						color="primary"
-						className={classes.submit}
-					>
-						{i18n.t("login.buttons.submit")}
-					</Button>
-					{ <Grid container>
-						<Grid item>
-							<Link
-								href="#"
-								variant="body2"
-								component={RouterLink}
-								to="/signup"
-							>
-								{i18n.t("login.buttons.register")}
-							</Link>
-						</Grid>
-					</Grid> }
-				</form>
-			
-			</div>
-			<Box mt={5}>
-				<Copyright />
-			</Box>
-		</Container>
-		</div>
-	);
+    return (
+        <div className={classes.root}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <div>
+                        <img style={{ margin: "0 auto", width: "100%" }} src={logo} alt="Whats" />
+                    </div>
+                    
+                    <form className={classes.form} noValidate onSubmit={formik.handleSubmit}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            id="email"
+                            label={i18n.t("login.form.email")}
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.email && Boolean(formik.errors.email)}
+                            helperText={formik.touched.email && formik.errors.email}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            name="password"
+                            label={i18n.t("login.form.password")}
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.password && Boolean(formik.errors.password)}
+                            helperText={formik.touched.password && formik.errors.password}
+                        />
+
+                        <Grid container justifyContent="flex-end">
+                            <Grid item xs={6} style={{ textAlign: "right" }}>
+                                <Link component={RouterLink} to="/forgetpsw" variant="body2">
+                                    Esqueceu sua senha?
+                                </Link>
+                            </Grid>
+                        </Grid>
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            {i18n.t("login.buttons.submit")}
+                        </Button>
+
+                        <Grid container>
+                            <Grid item>
+                                <Link href="#" variant="body2" component={RouterLink} to="/signup">
+                                    {i18n.t("login.buttons.register")}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+                <Box mt={5}>
+                    <Copyright />
+                </Box>
+            </Container>
+        </div>
+    );
 };
 
 export default Login;
