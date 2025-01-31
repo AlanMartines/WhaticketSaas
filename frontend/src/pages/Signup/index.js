d5833882792ad29aca699a61d4b848d529d011fd
Alan Martines <alancpmartines@hotmail.com>
2025-01-31 14:35:55 -0400

    Update

--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -143,7 +143,7 @@ const SignUp = () => {
 
 	return (
 		<div className={classes.root}>
-			<Container component="main" maxWidth="xs">
+			<Container component="main" maxWidth="md">
 				<CssBaseline />
 				<div className={classes.paper}>
 					<img className={classes.logo} src={logo} alt="Logo" />
