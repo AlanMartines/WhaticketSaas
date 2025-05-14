a550cf47e9dbb2c5f58b7ebc9019579be53d5301
Alan Martines <alancpmartines@hotmail.com>
2025-05-14 09:30:00 -0400

    Update

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,13 @@
 //simple express server to run frontend production build;
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const app = express();
+const { logger } = require("./src/utils/logger");
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(3000);
-
+app.listen(process.env.PORT, async () => {
+  logger.info(`Server started on port: ${process.env.PORT}`);
+});
